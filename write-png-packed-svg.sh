a936e6f0e35ba6f181598f2264146efe0f6f7f7c
Jonathan-David Schröder <jonathan.schroder@gmail.com>
2020-10-18 15:14:50 +0200

    #1 #6 ditching bash cli script in favour of python popen

--- write-png-packed-svg.sh
+++
@@ -1,8 +0,0 @@
-echo $#
-if test $# -ne 2; then
-  echo Usage: $0 input.svg output-filename-without-extension
-  exit 0
-fi
-inkscape $1 -C -o $2.png
-inkscape $1 --export-text-to-path --export-filename=$2.svg
-ls $2*
